ca5bfe908901ab796fe3d46c4d7be3e3f9bcf4cd
Mathieu Larue <mlarue@mirego.com>
2022-12-07 22:59:27 -0500

    Fix

--- advent7/src/main/kotlin/Main.kt
+++ advent7/src/main/kotlin/Main.kt
@@ -3,26 +3,31 @@ import java.io.File
 interface IFile {
     val name: String
     val size: Int
-    val parent: IFile?
+    val parent: Directory?
 }
 
 data class SingleFile(
     override val name: String,
     override val size: Int,
-    override val parent: IFile?
+    override val parent: Directory?
 ) : IFile
 
 data class Directory(
     override val name: String,
-    override var parent: IFile?
+    override var parent: Directory?
 ) : IFile {
     var files: MutableMap<String, IFile> = mutableMapOf()
-    override val size: Int = files.values.sumOf { it.size }
+    override val size: Int
+        get() = files.values.sumOf { it.size }
 }
 
 fun main(args: Array<String>) {
     val lines = File("input.txt").readLines()
 
+    val root = parseTree(lines)
+}
+
+fun parseTree(lines: List<String>): Directory {
     var index = 0
 
     var directory: Directory? = null
@@ -36,14 +41,21 @@ fun main(args: Array<String>) {
             "$" -> {
                 when (parts[1]) {
                     "cd" -> {
-                        directory = directory?.files?.values?.firstOrNull { it is Directory && it.name == parts[2] } as? Directory ?: run {
-                            Directory(parts[0], directory).also {
-                                if (root == null) {
-                                    root = it
-                                }
-                            }
+                        if (parts[2] == "..") {
+                            directory = directory?.parent
+                        } else {
+                            directory =
+                                directory?.files?.values?.firstOrNull { it is Directory && it.name == parts[2] } as? Directory
+                                    ?: run {
+                                        Directory(parts[0], directory).also {
+                                            if (root == null) {
+                                                root = it
+                                            }
+                                        }
+                                    }
                         }
                     }
+
                     "ls" -> {
                         var tempIndex = index
                         var tempParts: List<String> = emptyList()
@@ -56,7 +68,10 @@ fun main(args: Array<String>) {
                                 when (tempParts[0]) {
                                     "$" -> index = tempIndex - 1
                                     "dir" -> directory?.files?.set(tempParts[1], Directory(tempParts[1], directory))
-                                    else -> directory?.files?.set(tempParts[1], SingleFile(tempParts[1], tempParts[0].toInt(), directory))
+                                    else -> directory?.files?.set(
+                                        tempParts[1],
+                                        SingleFile(tempParts[1], tempParts[0].toInt(), directory)
+                                    )
                                 }
                             }
                         } while (tempIndex in lines.indices && tempParts[0] != "$")
@@ -67,4 +82,6 @@ fun main(args: Array<String>) {
 
         ++index
     } while (index in lines.indices)
+
+    return root!!
 }
