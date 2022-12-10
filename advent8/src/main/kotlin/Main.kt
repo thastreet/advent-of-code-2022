b6bd899efc6b0db5f4cea1a8326766f69ec672db
Mathieu Larue <mlarue@mirego.com>
2022-12-10 14:39:02 -0500

    Part 2

--- advent8/src/main/kotlin/Main.kt
+++ advent8/src/main/kotlin/Main.kt
@@ -7,6 +7,9 @@ fun main(args: Array<String>) {
 
     val part1Answer = part1(trees)
     println("part1Answer: $part1Answer")
+
+    val part2Answer = part2(trees)
+    println("part2Answer: $part2Answer")
 }
 
 fun parseTrees(lines: List<String>): Array<Array<Int>> =
@@ -40,4 +43,51 @@ fun part1(trees: Array<Array<Int>>): Int {
     }
 
     return count
+}
+
+fun part2(trees: Array<Array<Int>>): Int {
+    var highest = 0
+
+    for (j in trees.indices) {
+        for (i in trees[0].indices) {
+            val row = trees[j]
+            val column = Array(trees.size) {
+                trees[it][i]
+            }
+
+            val tree = trees[j][i]
+
+            var top = 0
+            var bottom = 0
+            var left = 0
+            var right = 0
+
+            for (currentJ in j - 1 downTo 0) {
+                ++top
+                if (column[currentJ] >= tree) break
+            }
+
+            for (currentJ in j + 1 until column.size) {
+                ++bottom
+                if (column[currentJ] >= tree) break
+            }
+
+            for (currentI in i - 1 downTo 0) {
+                ++left
+                if (row[currentI] >= tree) break
+            }
+
+            for (currentI in i + 1 until row.size) {
+                ++right
+                if (row[currentI] >= tree) break
+            }
+
+            val score = top * bottom * left * right
+            if (score > highest) {
+                highest = score
+            }
+        }
+    }
+
+    return highest
 }
